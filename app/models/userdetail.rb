63b8935f1f7b474b5d0378926660fa96ce5bd56d
Darren <darren.dalino30@gmail.com>
2016-04-18 23:29:05 +0800

    change

--- app/models/userdetail.rb
+++ app/models/userdetail.rb
@@ -8,10 +8,13 @@ class Userdetail < ActiveRecord::Base
 		if search.present?
 			keyword = search
 			split = keyword.split(" ")
-			if split.to_a.count > 1
-			where("first_name iLike :q or middle_name iLike :q ", q: split[0],  q: split[1]) 
-			else
-				where("first_name iLike :q ", q: split[0])
+			if split.to_a.count >= 1
+				where("first_name @@ :q ", q: split[0])	
+			elsif split.to_a.count >= 1
+				where("first_name @@ :q or middle_name @@ :q ", q: split[0], q: split[1])
+			elsif split.to_a.count >= 1
+				where("first_name @@ :q or middle_name @@ :q or last_name @@ :q", q: split[0],  q: split[1], q: split[2]) 
+				
 			end
 		else
 			all
