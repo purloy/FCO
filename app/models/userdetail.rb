94f35b1910909132e17fd3b324c4c60f8145bf66
Darren <darren.dalino30@gmail.com>
2016-04-19 00:12:12 +0800

    changes

--- app/models/userdetail.rb
+++ app/models/userdetail.rb
@@ -4,18 +4,37 @@ class Userdetail < ActiveRecord::Base
 	belongs_to :mcluster
 
 	def self.search(search)
-		
+			a = where("first_name @@ :q ", q: search)
+			b = where("middle_name @@ :q ", q: search)
+			c = where("last_name @@ :q ", q: search)
+
 		if search.present?
 			keyword = search
 			split = keyword.split(" ")
-			if split.to_a.count >= 1
-				where("first_name @@ :q ", q: split[0])	
+			if a.to_a.count == 1
+				where("first_name @@ :q ", q: search)
+			elsif b.to_a.count == 1
+				where("middle_name @@ :q ", q: search)
+			elsif c.to_a.count == 1
+				where("last_name @@ :q ", q: search)
 			elsif split.to_a.count >= 1
 				where("first_name @@ :q or middle_name @@ :q ", q: split[0], q: split[1])
 			elsif split.to_a.count >= 1
 				where("first_name @@ :q or middle_name @@ :q or last_name @@ :q", q: split[0],  q: split[1], q: split[2]) 
-				
+			else
+				a
 			end
+			#keyword = search
+			#split = keyword.split(" ")
+			#if split.to_a.count >= 1
+			#	where("first_name @@ :q ", q: split[0])	
+			#elsif split.to_a.count >= 1
+			#	where("first_name @@ :q or middle_name @@ :q ", q: split[0], q: split[1])
+			#elsif split.to_a.count >= 1
+			#	where("first_name @@ :q or middle_name @@ :q or last_name @@ :q", q: split[0],  q: split[1], q: split[2]) 
+			#	else
+			#		all
+			#end
 		else
 			all
 		end
