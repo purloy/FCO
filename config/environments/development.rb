917531d8e93828f3eddf74ea3524ef937bbe9abf
Darren <darren.dalino30@gmail.com>
2016-04-18 11:59:37 +0800

    changes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -50,6 +50,22 @@ Rails.application.configure do
     #   :enable_starttls_auto   => true
 
   #}
+  
+  config.action_mailer.default_url_options = { :host => 'calm-caverns-77486.herokuapp.com' }  
+  config.action_mailer.delivery_method = :smtp  
+  config.action_mailer.perform_deliveries = true  
+  config.action_mailer.raise_delivery_errors = false  
+  config.action_mailer.default :charset => "utf-8"  
+  config.action_mailer.smtp_settings = {  
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "calm-caverns-77486.herokuapp.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: 'darren.dalino30@gmail.com',
+    password: 'akocdarren1'
+  }
+
 
   
  
