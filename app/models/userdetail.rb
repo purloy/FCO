047d2bfe54f7a919551c301afa1dedc9b93a616e
Darren <darren.dalino30@gmail.com>
2016-04-19 12:22:22 +0800

    cleaning

--- app/models/userdetail.rb
+++ app/models/userdetail.rb
@@ -4,9 +4,6 @@ class Userdetail < ActiveRecord::Base
 	belongs_to :mcluster
 
 	def self.search(search)
-			a = where("first_name @@ :q ", q: search)
-			b = where("middle_name @@ :q ", q: search)
-			c = where("last_name @@ :q ", q: search)
 
 		if search.present?
 			where("CONCAT_WS(' ', first_name, middle_name, last_name) @@ :q", q: "%#{search}%")
